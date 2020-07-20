ef8db9e83347852195d13c42ca247d1f66690d11
Chris Eadie <chris.eadie@outlook.com>
2020-07-20 17:24:06 +0100

    Add scrolling to view and fix genre text slipping outside of image

--- Bookworm/Views/BookDetailView.swift
+++ Bookworm/Views/BookDetailView.swift
@@ -13,29 +13,31 @@ struct BookDetailView: View {
   
   var body: some View {
     GeometryReader { geometry in
-      VStack {
-        ZStack(alignment: .bottomTrailing) {
-          Image(self.book.genre ?? "FANTASY")
+      ScrollView {
+        VStack {
+          ZStack(alignment: .bottomTrailing) {
+            Image(self.book.genre ?? "FANTASY")
+            Text(self.book.genre?.uppercased() ?? "FANTASY")
+              .font(.caption)
+              .fontWeight(.black)
+              .padding(8)
+              .foregroundColor(.white)
+              .background(Color.black.opacity(0.75))
+              .clipShape(Capsule())
+              .offset(x: -5, y: -5)
+          }
             .frame(maxWidth: geometry.size.width)
-          Text(self.book.genre?.uppercased() ?? "FANTASY")
-            .font(.caption)
-            .fontWeight(.black)
-            .padding(8)
-            .foregroundColor(.white)
-            .background(Color.black.opacity(0.75))
-            .clipShape(Capsule())
-            .offset(x: -5, y: -5)
+          Text(self.book.author ?? "Unknown author")
+            .font(.title)
+          RatingView(rating: .constant(Int(self.book.rating)))
+            .font(.largeTitle)
+          Text(self.book.review ?? "No review")
+            .padding()
+          Spacer()
         }
-        Text(self.book.author ?? "Unknown author")
-          .font(.title)
-        RatingView(rating: .constant(Int(self.book.rating)))
-          .font(.largeTitle)
-        Text(self.book.review ?? "No review")
-          .padding()
-        Spacer()
       }
     }
-    .navigationBarTitle(Text(book.title ?? "Unknown book"), displayMode: .inline)
+    .navigationBarTitle(Text(book.title ?? "Unknown book")/*, displayMode: .inline*/)
   }
 }
 
